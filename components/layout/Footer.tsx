a7096b8a3f24e44368e865bf35ff048c0a665b02
Matheus <0001064667@senaimgaluno.com.br>
2026-04-28 21:47:34 -0300

    chore: adiciona meu nome no Footer

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -9,7 +9,7 @@
  */
 const equipe: { nome: string; github: string }[] = [
   // ====== INÍCIO DA ÁREA DE EDIÇÃO COLABORATIVA ======
-
+{ nome: "Matheus Afonso Batista", github: "Matheuszzzx" },
   // ====== FIM DA ÁREA DE EDIÇÃO COLABORATIVA ======
 ];
 
