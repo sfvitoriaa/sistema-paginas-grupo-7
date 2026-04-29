c1aba5318c9c05d07ed36c0bac79bf1b9fb6870b
Matheus <0001064667@senaimgaluno.com.br>
2026-04-28 21:43:54 -0300

    feat: adiciona link Sobre Navbar

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -13,7 +13,7 @@ import { cn } from "@/lib/utils";
  */
 const navLinks: { href: string; label: string }[] = [
   // ====== INÍCIO DA ÁREA DE EDIÇÃO COLABORATIVA ======
-
+                { href: "/sobre", label: "Sobre" },
   // ====== FIM DA ÁREA DE EDIÇÃO COLABORATIVA ======
 ];
 
