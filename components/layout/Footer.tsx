0dda58a3e19f3cd55601ce30076df88327b4b2cd
gustavobarcelos18 <gustavobarcelos699@gmail.com>
2026-04-28 21:28:26 -0300

    chore: adicionando meu nome no Footer

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -9,7 +9,7 @@
  */
 const equipe: { nome: string; github: string }[] = [
   // ====== INÍCIO DA ÁREA DE EDIÇÃO COLABORATIVA ======
-
+{ nome: "Gustavo Barcelos", github: "gustavobarcelos18" },
   // ====== FIM DA ÁREA DE EDIÇÃO COLABORATIVA ======
 ];
 
