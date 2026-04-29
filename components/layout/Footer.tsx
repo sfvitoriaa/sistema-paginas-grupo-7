765a20bdc0cc25f0b19ddae70dfa2408e5cd24ff
sofia <0001046198@senaimgaluno.com.br>
2026-04-28 21:49:09 -0300

    chore: adicionar meu nome no Footer

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -9,7 +9,7 @@
  */
 const equipe: { nome: string; github: string }[] = [
   // ====== INÍCIO DA ÁREA DE EDIÇÃO COLABORATIVA ======
-
+{nome: "Sofia Vitória", github: "sfvitoriaa"},
   // ====== FIM DA ÁREA DE EDIÇÃO COLABORATIVA ======
 ];
 
